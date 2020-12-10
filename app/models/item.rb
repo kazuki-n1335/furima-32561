c9aa6ce65655f921854ccdc1e657532a4ae10606
kazuki-n1335 <kazuki.240.1337@gmail.com>
2020-12-10 15:58:04 +0900

    ビューファイルのフォームの内容記述とサーバーを起動した際のエラー修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,14 +9,14 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :was_attached?
+  validates :was_attached?, presence: true
   validates :name, presence: true, length: { maximum: 40 }
   validates :introduce, presence: true, length: { maximum: 1000 }
-  validates :category_id, { other_than: 0, message: "can't be blank" }
-  validates :item_status_id, { other_than: 0, message: "can't be blank" }
-  validates :delivery_fee_id, { other_than: 0, message: "can't be blank" }
-  validates :shipping_area_id, { other_than: 0, message: "can't be blank" }
-  validates :shipping_days_id, { other_than: 0, message: "can't be blank" }
+  validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :item_status_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :delivery_fee_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :shipping_area_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :shipping_days_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :value, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 
   def was_attached?

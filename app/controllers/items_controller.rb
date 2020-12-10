e177ddd6cae17f959fd046a638b2709ea776b4d8
kazuki-n1335 <kazuki.240.1337@gmail.com>
2020-12-10 14:59:16 +0900

    new,createアクション内容とストロングパラメータを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
   end
-
-  def create
-    
+  
+  def createitem
+    @item = Item.new(prototypes_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   #item_paramsのpermit内に:imageも必ず記入
+  private
+
+  def message_params
+    params.require(:item).permit(:name, :introduce, :category_id, :item_status_id, :delivery_fee_id, :shipping_area_id, :shipping_days_id, :value, :image).merge(user_id: current_user.id)
+  end
 end

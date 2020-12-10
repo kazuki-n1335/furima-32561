65476f0ee67646fc1a61cb4e16c67ef60363a37a
kazuki-n1335 <kazuki.240.1337@gmail.com>
2020-12-10 18:59:10 +0900

    ログイン時のみ新規投稿ページに遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: :index
+
   def index
   end
 
@@ -14,10 +16,16 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-  #item_paramsのpermit内に:imageも必ず記入
+  
   private
 
   def item_params
     params.require(:item).permit(:name, :introduce, :category_id, :item_status_id, :delivery_fee_id, :shipping_area_id, :shipping_day_id, :value, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end

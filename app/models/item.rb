6368e654cd479c5af231b8b9efa7532c966d96ae
kazuki-n1335 <kazuki.240.1337@gmail.com>
2020-12-10 14:13:10 +0900

    ActiveHashのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :delivery_fee
+  belongs_to :shipping_area
+  belongs_to :shipping_days
 
   belongs_to :user
   has_one_attached :image

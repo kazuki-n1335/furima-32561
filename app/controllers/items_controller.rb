0b8b2cc9a08c4fdb85c68eb6521530b23b693202
kazuki-n1335 <kazuki.240.1337@gmail.com>
2020-12-11 11:32:34 +0900

    ログインページへのリダイレクト修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_sign_in, except: :index
+  before_action :authenticate_user!, except: :index
 
   def index
   end
@@ -23,7 +23,4 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :introduce, :category_id, :item_status_id, :delivery_fee_id, :shipping_area_id, :shipping_day_id, :value, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_sign_in
-    redirect_to new_user_session_path unless user_signed_in?
-  end
 end

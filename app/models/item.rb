069c07ec64a720278670c25f31aa77c3fe3eb45f
kazuki-n1335 <kazuki.240.1337@gmail.com>
2020-12-11 11:39:07 +0900

    バリデーションをwith_optionsを用いた形に修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,14 +9,20 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :was_attached?, presence: true
-  validates :name, presence: true, length: { maximum: 40 }
-  validates :introduce, presence: true, length: { maximum: 1000 }
-  validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :item_status_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :delivery_fee_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :shipping_area_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :shipping_day_id, numericality: { other_than: 0, message: "can't be blank" }
+  with_options presence: true do
+    validates :was_attached?
+    validates :name, length: { maximum: 40 }
+    validates :introduce, length: { maximum: 1000 }
+  end
+  
+  with_options numericality: { other_than: 0, message: "can't be blank" } do
+    validates :category_id
+    validates :item_status_id
+    validates :delivery_fee_id
+    validates :shipping_area_id
+    validates :shipping_day_id
+  end
+
   validates :value, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
 
   def was_attached?

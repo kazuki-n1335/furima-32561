f2a4db1415fcacba7f27d5d6001b411f16351728
kazuki-n1335 <kazuki.240.1337@gmail.com>
2020-12-14 12:13:10 +0900

    Formオブジェクトのモデルの生成

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -9,4 +9,11 @@ class UserOrder
     validates :telephone_number, length: { maximum: 11 },  format: { with: /\A[0-9]/, message: 'is invalid. Input full-width numbers.' }
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+
+  belongs_to :user
+  belongs_to :item
+
+  def save
+    Address.create(post_number: post_number, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, telephone_number: telephone_number, user_id: user.id, item_id: item.id)
+  end
 end

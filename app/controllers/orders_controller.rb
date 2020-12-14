15b77de24640453d6907d136057d0b8bc83e9420
kazuki-n1335 <kazuki.240.1337@gmail.com>
2020-12-14 11:11:20 +0900

    ビューファイルの配置とコントローラールーティング設定

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,21 @@
+class OrdersController < ApplicationController
+  def index
+    @item = Item.find(params[:id])
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+end

a5a40e086ae73f6f4d279b032bb902fc33eb5c58
kazuki-n1335 <kazuki.240.1337@gmail.com>
2020-12-15 13:28:46 +0900

    条件分岐修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,6 +29,6 @@ class OrdersController < ApplicationController
 
   def move_to_root_path
     @item = Item.find(params[:item_id])
-    redirect_to root_path if @item.order.present?
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 end

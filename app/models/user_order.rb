39af68f41056c3691698c55515b149cae6363788
kazuki-n1335 <kazuki.240.1337@gmail.com>
2020-12-15 12:44:56 +0900

    テストコード実装

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -6,7 +6,7 @@ class UserOrder
     validates :post_number,format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :house_number
-    validates :telephone_number, length: { maximum: 11 },  format: { with: /\A[0-9]/, message: 'is invalid. Input full-width numbers.' }
+    validates :telephone_number, format: { with: /\A[0-9]/, message: 'is invalid. Input full-width numbers.' }, length: { maximum: 11 }
     validates :token
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }

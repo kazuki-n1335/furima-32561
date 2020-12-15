3b9c8ee6bfab9e3002e84a75e7321d02762f4693
kazuki-n1335 <kazuki.240.1337@gmail.com>
2020-12-15 11:31:34 +0900

    購入済み商品への条件分岐

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :move_to_root_path
   def index
-    @item = Item.find(params[:item_id])
     @user_order = UserOrder.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @user_order = UserOrder.new(order_params)
     if @user_order.valid?
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]   # 自身のPAY.JPテスト秘密鍵を記述しましょう
@@ -25,5 +25,12 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:user_order).permit(:post_number, :prefecture_id, :city, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def move_to_root_path
+    @item = Item.find(params[:item_id])
+    if @item.order.present?
+      redirect_to root_path
+    end
   end
 end

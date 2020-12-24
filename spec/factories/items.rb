2d4d457310c8c440073ff34cd10a722996d07255
kazuki-n1335 <kazuki.240.1337@gmail.com>
2020-12-24 14:42:38 +0900

    Update items.rb

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     introduce { Faker::Lorem.sentence }
     name { Faker::Games::Pokemon.name }
-    value { Faker::Number.between(300, 100_000) }
-    category_id { Faker::Number.between(1, 10) }
-    item_status_id { Faker::Number.between(1, 6) }
-    delivery_fee_id { Faker::Number.between(1, 2) }
-    shipping_area_id { Faker::Number.between(1, 47) }
-    shipping_day_id { Faker::Number.between(1, 3) }
+    value { Faker::Number.between(from: 300, to: 100000) }
+    category_id { Faker::Number.between(from: 1, to: 10) }
+    item_status_id { Faker::Number.between(from: 1, to: 6) }
+    delivery_fee_id { Faker::Number.between(from: 1, to: 2) }
+    shipping_area_id { Faker::Number.between(from: 1, to: 47) }
+    shipping_day_id { Faker::Number.between(from: 1, to: 3) }
 
     association :user
 

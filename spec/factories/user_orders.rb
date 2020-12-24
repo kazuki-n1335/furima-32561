1f5ed3126ed427885dbc7dd621917803dc3d0734
kazuki-n1335 <kazuki.240.1337@gmail.com>
2020-12-24 14:38:41 +0900

    Update user_orders.rb

--- spec/factories/user_orders.rb
+++ spec/factories/user_orders.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :user_order do
     token { 'tok_abcdefghijk00000000000000000' }
     post_number { '123-4567' }
-    prefecture_id { Faker::Number.between(1, 47) }
+    prefecture_id { Faker::Number.between(from: 1, to: 47) }
     city { Gimei.city.kanji }
     house_number { Faker::Number.number(digits: 4) }
     building_name { Faker::Games::Pokemon.name }
     telephone_number { Faker::Number.leading_zero_number(digits: 11) }
-    user_id { Faker::Number.between(1, 50) }
-    item_id { Faker::Number.between(1, 50) }
+    user_id { Faker::Number.between(from: 1, to: 50) }
+    item_id { Faker::Number.between(from: 1, to: 50) }
   end
 end
